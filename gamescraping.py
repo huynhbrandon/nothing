e0fa6508be61fb4f9ef2a7c2aaee0c431898b17b
huynhbrandon <35756597+huynhbrandon@users.noreply.github.com>
2018-08-18 20:25:18 -0700

    Added a table scraper

---
+++ gamescraping.py
@@ -0,0 +1,32 @@
+#! python3
+
+# downloads highest grossing franchise information
+
+import urllib.request, bs4, csv
+#request lib to download the page, bs4 to look at things cooler, csv to write to excel
+
+f = open("dataoutput.csv", "w", newline = '') #opens an output file to write to
+writer = csv.writer(f)          
+
+url = "https://en.wikipedia.org/wiki/List_of_highest-grossing_video_game_franchises"
+
+print('Downloading page...')
+
+res = urllib.request.urlopen(url) #downloads the page
+ # check that it works
+
+soupdata = bs4.BeautifulSoup(res.read(), 'lxml') #parsing
+
+table = soupdata.find('table', {"class":"wikitable sortable"})
+
+rows = table.findAll('tr')
+
+for tr in rows:
+        cols = tr.findAll(recursive = False)
+        cols = [ele.text.strip() for ele in cols]
+
+        writer.writerow(cols)
+        f.flush()
+        print(cols)
+
+f.close()
